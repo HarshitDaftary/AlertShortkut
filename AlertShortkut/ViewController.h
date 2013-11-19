5105ee82c0e19bbb82d566df1bdddd5c7cdca1d3
HDBaggy <harshit@Harshit-Daftary.local>
2013-11-19 21:53:52 +0530

    Initial Commit

---
+++ AlertShortkut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlertShortkut
+//
+//  Created by HDBaggy on 19/11/13.
+//  Copyright (c) 2013 Logic Engine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AlertShortkut/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlertShortkut
+//
+//  Created by HDBaggy on 19/11/13.
+//  Copyright (c) 2013 Logic Engine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
